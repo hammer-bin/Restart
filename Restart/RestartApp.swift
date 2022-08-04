529afe81393f16f6cf4cf2877997a8388335052b
suslmk <67575226+suslmk-lee@users.noreply.github.com>
2022-08-04 09:06:53 +0900

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by minkyuLee on 2022/08/04.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
